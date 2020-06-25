dcbc69f2d7557c1a056dea8f4ecdf136fadc0d12
Xander Dean <aaxelld@gmail.com>
2020-06-25 18:02:15 -0400

    goliath online

---
+++ controllers/data.php
@@ -0,0 +1,37 @@
+<?php
+include_once __DIR__ . '/../models/joke.php';
+
+header('Content-Type: application/json');
+
+
+if ($_REQUEST['action'] === 'index') {
+    // simple get request ... just load al the class factory objects
+  echo json_encode(datagroup::all());
+}
+
+elseif ($_REQUEST['action'] === 'post') {
+    // file_get_contents loads document as string
+  $request_body = file_get_contents('php://input');
+    // json_decode renders it as a JSON Object
+  $body_object = json_decode($request_body);
+    // uses model to create new class object
+  $new_data = new data(null, $body_object->name, $body_object->info);
+    // adds the new data to the datagroup class factory object
+  $all_data = datagroup::create($new_data);
+    // Finally --- we render it back in json string format
+  echo json_encode($all_jokes);
+}
+
+else if ($_REQUEST['action'] === 'update') {
+  $request_body = file_get_contents('php://input');
+  $body_object = json_decode($request_body);
+  $updated_data = new Data($_REQUEST['id'], $body_object->setup, $body_object->delivery);
+  $all_jokes = Jokes::update($updated_joke);
+  echo json_encode($all_jokes);
+}
+
+else if ($_REQUEST['action'] === 'delete') {
+  $all_jokes = Jokes::delete($_REQUEST['id']);
+  echo json_encode($all_jokes);
+}
+?>
---
+++ models/data.php
@@ -0,0 +1,86 @@
+<?php
+// Establish database Connection
+$dbconn = null;
+    // Access data configurations
+if(getenv('DATABASE_URL')){
+    $connectionConfig = parse_url(getenv('DATABASE_URL'));
+    $host = $connectionConfig['host'];
+    $user = $connectionConfig['user'];
+    $password = $connectionConfig['pass'];
+    $port = $connectionConfig['port'];
+    $dbname = trim($connectionConfig['path'],'/');
+    $dbconn = pg_connect(
+        "host=".$host." ".
+        "user=".$user." ".
+        "password=".$password." ".
+        "port=".$port." ".
+        "dbname=".$dbname
+    );
+} else {
+    // Local Host Database
+    $dbconn = pg_connect("host=localhost dbname=jokes user=postgres password=blackdra9891");
+}
+
+// Data Object Model
+class Data {
+  public $name;
+  public $info;
+  public $id;
+    // Construct object properties
+  public function __construct($id, $name, $info){
+    $this->id = $id;
+    $this->name = $name;
+    $this->info = $info;
+  }
+}
+
+// Create Class Factory to build out each object
+class DataGroup {
+  static function all(){
+    $datagroup = array();
+    // Select all
+    $results = pg_query("SELECT * FROM datagroup");
+
+    $row_object = pg_fetch_object($results);
+
+    // Build out class for each object in the query.
+    while($row_object){
+      $new_data = new Data(
+        intval($row_object->id),
+        $row_object->name,
+        $row_object->info
+      );
+    //   add new data row to data group
+      $datagroup[] = $new_data;
+      $row_object = pg_fetch_object($results);
+    }
+        // return the group object
+    return $datagroup;
+  }
+
+//   Handle create requests
+  static function create($data){
+    $query = "INSERT INTO datagroup (name, info) VALUES ($1, $2)";
+    $query_params = array($data->name, $data->info);
+    pg_query_params($query, $query_params);
+    return self::all();
+  }
+
+//   Update / Put Request
+  static function update($updated_data){ 
+      $query = "UPDATE datagroup SET name = $1, info = $2 WHERE id = $3";
+      $query_params = array($updated_data->name, $updated_data->info, $updated_data->id);
+      $result = pg_query_params($query, $query_params);
+
+      return self::all();
+    }
+//  Delete by ID request
+    static function delete($id){
+      $query = "DELETE FROM datagroup WHERE id = $1";
+      $query_params = array($id);
+      $result = pg_query_params($query, $query_params);
+
+      return self::all();
+    }
+}
+?>
